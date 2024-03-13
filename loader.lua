e265edac134e5629a1d2b7296da15e230921cfb9
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-03-14 02:40:19 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
-
+local games = loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/Voxul/VoxulHub/main/games.lua')))()
+if games[game.PlaceId] then loadstring(game:HttpGetAsync(games[game.PlaceId]))() else warn("Voxul currently does not support this experience!") end
