b3bc4db3d3d4d108772d2c699cef38a01da3ac44
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-03-19 02:18:24 +0800

    Update SlapRoyale_9431156611.lua

--- games/SlapRoyale_9431156611.lua
+++ games/SlapRoyale_9431156611.lua
@@ -521,19 +521,20 @@ Tab_Misc:AddToggle({
 	Callback = function(v)
 		lobbyhiding = v
 		if not v or not workspace:FindFirstChild("Lobby") then return end
-		
-		lobbyViewerPart.Position = HumanoidRootPart.Position
-		local cframe = HumanoidRootPart.CFrame
-		while lobbyhiding and workspace:FindFirstChild("Lobby") do
-			workspace.CurrentCamera.CameraSubject = lobbyViewerPart
-			pivotModelTo(Character, cframe + Vector3.new(math.random(), 150, math.random()), true)
-			task.wait()
-		end
-		workspace.CurrentCamera.CameraSubject = Humanoid
-		pivotModelTo(Character, cframe, true)
+		task.spawn(function()
+			lobbyViewerPart.Position = HumanoidRootPart.Position
+			local cframe = HumanoidRootPart.CFrame
+			while lobbyhiding and workspace:FindFirstChild("Lobby") do
+				workspace.CurrentCamera.CameraSubject = lobbyViewerPart
+				pivotModelTo(Character, cframe + Vector3.new(math.random(), 150, math.random()), true)
+				task.wait()
+			end
+			workspace.CurrentCamera.CameraSubject = Humanoid
+			pivotModelTo(Character, cframe, true)
+		end)
 	end,
 	Save = true,
-	Flag = "LocalCharLobbyHide"
+	Flag = "LobbyHider"
 })
 
 local AutoVotekicker = Tab_Misc:AddSection({
