ce4f50cb650008d784d83b7f00ee0d7fbc12fb30
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-03-18 22:33:29 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Voxul/VoxulHub/main/games.lua'))()
 if games[game.PlaceId] then
   local getgenv = getgenv or getfenv
-  getgenv().VoxulLib = 'https://raw.githubusercontent.com/shlexware/Orion/main/source'
+  getgenv().VoxulLib = 'https://raw.githubusercontent.com/Voxul/VoxulHub/main/Orion.lua'
   getgenv().VoxulWindowCONF = {Name = "Voxul Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Voxul_ORIONLIB", IntroEnabled = true, IntroText = "Voxul Hub", IntroIcon = "http://www.roblox.com/asset/?id=6035039429"}
   loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
 else 
