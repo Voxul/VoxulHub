fe6d04b591bb7c04893d01fd48c5751bd3165ae8
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-03-19 02:35:15 +0800

    Update SlapRoyale_9431156611.lua

--- games/SlapRoyale_9431156611.lua
+++ games/SlapRoyale_9431156611.lua
@@ -516,24 +516,22 @@ lobbyViewerPart.Transparency = 1
 lobbyViewerPart.CanCollide = false
 local lobbyhiding = false
 Tab_Misc:AddToggle({
-	Name = "Hide in lobby",
+	Name = "Lobby hider",
 	Default = false,
+	Save = true,
 	Callback = function(v)
 		lobbyhiding = v
 		if not v or not workspace:FindFirstChild("Lobby") then return end
-		task.spawn(function()
-			lobbyViewerPart.Position = HumanoidRootPart.Position
-			local cframe = HumanoidRootPart.CFrame
-			while lobbyhiding and workspace:FindFirstChild("Lobby") do
-				workspace.CurrentCamera.CameraSubject = lobbyViewerPart
-				pivotModelTo(Character, cframe + Vector3.new(math.random(), 150, math.random()), true)
-				task.wait()
-			end
-			workspace.CurrentCamera.CameraSubject = Humanoid
-			pivotModelTo(Character, cframe, true)
-		end)
+		lobbyViewerPart.Position = HumanoidRootPart.Position
+		local cframe = HumanoidRootPart.CFrame
+		while lobbyhiding and workspace:FindFirstChild("Lobby") do
+			workspace.CurrentCamera.CameraSubject = lobbyViewerPart
+			pivotModelTo(Character, cframe + Vector3.new(math.random(), 150, math.random()), true)
+			task.wait()
+		end
+		workspace.CurrentCamera.CameraSubject = Humanoid
+		pivotModelTo(Character, cframe, true)
 	end,
-	Save = true,
 	Flag = "LobbyHider"
 })
 
