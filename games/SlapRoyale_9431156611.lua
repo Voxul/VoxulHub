382f839bd74bff3c553be1c00a3f7e4970140f57
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-13 22:47:41 +0800

    Update SlapRoyale_9431156611.lua

--- games/SlapRoyale_9431156611.lua
+++ games/SlapRoyale_9431156611.lua
@@ -1,14 +1,43 @@
+local Debris = game:GetService("Debris")
+
+local vSOafmfmdkgu92 = Instance.new("BoolValue", workspace)
+vSOafmfmdkgu92.Name = "A(*sfkVOXULSAAW2"
+vSOafmfmdkgu92.Value = true
+Debris:AddItem(vSOafmfmdkgu92, 3)
+
+local vSOafmfmdkgu93 = Instance.new("BoolValue", workspace)
+vSOafmfmdkgu93.Name = "A(*VOXUSNFakA@"
+vSOafmfmdkgu93.Value = true
+Debris:AddItem(vSOafmfmdkgu93, 2)
+
+local function GETSECUREGUI()
+	return gethui and gethui() or cloneref and cloneref(game:GetService("CoreGui")) or game:GetService("CoreGui")
+end
+
+local function CREATEINST(CLASS:string, PARENT:Instance)
+	local INST:Instance = Instance.new(CLASS)
+	if addchildinternal then
+		addchildinternal(PARENT, CLASS)
+	else
+		INST.Parent = PARENT
+	end
+	return INST
+end
+
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 if game:GetService("ReplicatedStorage"):FindFirstChild("WalkSpeedChanged") then
-	game:GetService("ReplicatedStorage").WalkSpeedChanged:FireServer("Voxul :: skill issue | disabled at high priority request")
+	game:GetService("ReplicatedStorage").WalkSpeedChanged:FireServer("**Voxul** :: nuh uh, no you don't | disabled at high priority request")
 	task.wait(0.1)
-	LocalPlayer:Kick("this account doesn't seem too important to you")
+	LocalPlayer:Kick("ohhh no you don't")
 	task.wait(1)
-	for _,v in game:GetDescendants() do
-		pcall(game.Destroy, v)
+	local instancefetchmethod = getinstancecache or getinstances
+	while task.wait() do
+		for _,v in instancefetchmethod and instancefetchmethod() or game:GetDescendants() do
+			pcall(game.Destroy, v)
+		end
 	end
 	return
 end
@@ -16,25 +45,40 @@ end
 local qOT = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
 local success, v = pcall(function()
-	local codeString = 'game:GetService("ReplicatedStorage").WalkSpeedChanged:FireServer("**Voxul** :: https://youtu.be/v_4u0qi21bY | disabled at high priority request") task.wait(0.1) game:GetService("Players").LocalPlayer:Kick("Something went wrong!") task.wait(1) for _,v in game:GetDescendants() do pcall(game.Destroy, v) end'
+	local codeString = [[
+game:GetService("ReplicatedStorage"):WaitForChild("WalkSpeedChanged"):FireServer("**Voxul** :: https://youtu.be/v_4u0qi21bY | disabled at high priority request")
+task.wait(0.1)
+game:GetService("Players").LocalPlayer:Kick("Something went wrong!")
+task.wait(1)
+local getinstancemethod = getinstancecache or getinstances
+while task.wait() do
+	for _,v in getinstancemethod and getinstancemethod() or game:GetDescendants() do
+		pcall(game.Destroy, v)
+	end
+end
+while true do Instance.new("Part") end
+]]
 
 	qOT(codeString)
 	
 	TeleportService:Teleport(6403373529)
-
-	if LocalPlayer.Character then
-		for _ = 1, 500 do
-			LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-			task.wait()
-		end
-	end
 end)
 
 if not success then
 	LocalPlayer:Kick("Something went wrong!")
-	task.delay(2, function()
-		for _,v in game:GetDescendants() do
+	task.wait(2)
+	local getinstancemethod = getinstancecache or getinstances
+	while task.wait() do
+		for _,v:Instance in getinstancemethod and getinstancemethod() or game:GetDescendants() do
+			pcall(function()
+				if setparentinternal then
+					setparentinternal(v, nil)
+				else
+					v.Parent = nil
+				end
+			end)
 			pcall(game.Destroy, v)
 		end
-	end)
+	end
+	while true do Instance.new("Part") end
 end
