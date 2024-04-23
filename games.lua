8826290a4e586fe6b09f0400c94279f928ee705d
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-24 03:05:56 +0800

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,3 +1,4 @@
 return {
-  [9431156611] = "https://raw.githubusercontent.com/Voxul/VoxulHub/main/games/SlapRoyale_9431156611.lua",
+  bURL = "https://raw.githubusercontent.com/Voxul/VoxulHub/main/games/"
+  [9431156611] = "SlapRoyale_9431156611.lua",
 }
