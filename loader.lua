c63bfa49f4944e928285fb9b3ad31c762cd0c78e
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-24 03:06:17 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ if games[game.PlaceId] then
   local getgenv = getgenv or getfenv
   getgenv().VoxulLib = 'https://raw.githubusercontent.com/Voxul/VoxulHub/main/Orion.lua'
   getgenv().VoxulWindowCONF = {Name = "Voxul Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Voxul_ORIONLIB", IntroEnabled = false, IntroText = "Voxul Hub", IntroIcon = "http://www.roblox.com/asset/?id=6035039429"}
-  loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
+  loadstring(game:HttpGetAsync(games.bURL .. games[game.PlaceId]))() 
 else 
   warn("Voxul currently does not support this experience!") 
 end
