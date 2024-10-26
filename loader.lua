932345773c0d0c6e54feacfedf4ccaf8fd21ba71
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-10-26 12:38:37 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,21 @@
+local unsecureEXEC = {
+  "krnl",
+  "delta"
+}
+
+local thisExecutor = identifyexecutor and string.lower(identifyexecutor())
+if not thisExecutor then
+  game:GetService("Players").LocalPlayer:Kick("Your executor is not supported!")
+  return
+end
+
+for _, exec in unsecureEXEC do
+  if string.match(thisExecutor, exec) then
+    game:GetService("Players").LocalPlayer:Kick("Your executor is UNSAFE/DETECTED!!! Please use a different executor!")
+    return
+  end
+end
+
 local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Voxul/VoxulHub/main/games.lua'))()
 if games[game.PlaceId] then
   local getgenv = getgenv or getfenv
@@ -5,5 +23,5 @@ if games[game.PlaceId] then
   getgenv().VoxulWindowCONF = {Name = "Voxul Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Voxul_ORIONLIB", IntroEnabled = false, IntroText = "Voxul Hub", IntroIcon = "http://www.roblox.com/asset/?id=6035039429"}
   loadstring(game:HttpGet(games.bURL .. games[game.PlaceId]))() 
 else 
-  warn("Voxul currently does not support this experience!") 
+  warn("Voxul currently does not support this experience!")
 end
