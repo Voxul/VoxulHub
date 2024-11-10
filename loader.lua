ff0cad6a5a06fac9b800f45d5cf8d18537071542
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-11-10 16:12:55 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,21 +1,3 @@
-local unsecureEXEC = {
-  "krnl",
-  "delta"
-}
-
-local thisExecutor = identifyexecutor and string.lower(identifyexecutor())
-if not thisExecutor then
-  game:GetService("Players").LocalPlayer:Kick("Your executor is not supported!")
-  return
-end
-
-for _, exec in unsecureEXEC do
-  if string.match(thisExecutor, exec) then
-    game:GetService("Players").LocalPlayer:Kick("Your executor is UNSAFE/DETECTED!!! Please use a different executor!")
-    return
-  end
-end
-
 local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Voxul/VoxulHub/main/games.lua'))()
 if games[game.PlaceId] then
   local getgenv = getgenv or getfenv
